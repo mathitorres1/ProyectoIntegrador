dac6c6a38183c17ebeb92d31a777abaeb8fb18c7
Mathias <mathiastorres7@gmail.com>
2022-08-15 15:21:42 -0300

    Seccion noticias
    commit agregando la seccion noticias usando css y jquery

--- code/code.js
+++ code/code.js
@@ -7,3 +7,10 @@ document
 document.querySelector(".close-popup").addEventListener("click", function () {
   document.querySelector(".popup").style.display = "none";
 });
+
+$('.newsSlick').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: true
+})
