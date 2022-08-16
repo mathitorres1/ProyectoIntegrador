635ff8c70450387e087ec814b352a457bdc8b48c
MenheraGirl <35434208+UmeBelle-P@users.noreply.github.com>
2022-08-16 11:34:21 -0300

    Se termina el css para el pop-up y se añade el footer (HTML y CSS)

--- code/code.js
+++ code/code.js
@@ -2,6 +2,7 @@ document
   .getElementById("registerPopupButton")
   .addEventListener("click", function () {
     document.querySelector(".popup").style.display = "flex";
+    document.querySelector(".popup-content").style.animation = "fade-in 1s ease forwards";
   });
 
 document.querySelector(".close-popup").addEventListener("click", function () {
